58bec930bbe77614f9f21dcd2c19538cf68bab91
Kazuki Mitomi <TomichibaGit@gmail.com>
2025-11-28 11:20:11 +0900

    fix and add test.bash

--- test.bash
+++ test.bash
@@ -1,25 +1,37 @@
-#!/bin/bash -xv
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2025 Kazuki Mitomi
 # SPDX-License-Identifier: GPL-3.0-only
+
 ng () {
-        echo ${1}行目が違うよ
-        res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
 
-out=$(seq 北海道 | ./prefecture)
-[ "${out}" = 0 ] || ng "$LINENO"
+out=$(echo "北海道" | ./prefecture)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "$out" = "北海道 の魅力度ランキングは 1 位です" ] || ng "$LINENO"
 
+out=$(echo "北海道
+沖縄県" | ./prefecture)
+[ "$?" = 0 ] || ng "$LINENO"
 
-out=$(echo あ | ./prefecture)
+expected="北海道 の魅力度ランキングは 1 位です
+沖縄県 の魅力度ランキングは 3 位です"
+
+[ "$out" = "$expected" ] || ng "$LINENO"
+
+
+out=$(echo "あ" | ./prefecture)
 [ "$?" = 1 ] || ng "$LINENO"
-[ "$out" = "あは 都道府県名ではありません" ] || ng "$LINENO"
+[ "$out" = "あ は 都道府県名ではありません" ] || ng "$LINENO"
 
-out=$(echo ""| ./prefecture)
+out=$(echo "" | ./prefecture)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+
 [ "${res}" = 0 ] && echo OK
 exit $res
+
